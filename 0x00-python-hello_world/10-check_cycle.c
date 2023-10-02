5bc1c3c5ab09e894f72f88e6552014ac0ab345ba
bayizere <bayizeremarius119@gmail.com>
2023-10-02 07:43:00 -0700

    0x00. Python - Hello, World

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: a pointer to the head of the linked list
+ * Return:
+ * 1 if a cycle is detected, 0 otherwise.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *burlnely = list;
+	listint_t *sleep = list;
+
+	if (!list)
+		return (0);
+
+	while (burlnely && sleep && sleep->next)
+	{
+		burlnely = burlnely->next;
+		sleep = sleep->next->next;
+		if (burlnely == sleep)
+			return (1);
+	}
+
+	return (0);
+}
