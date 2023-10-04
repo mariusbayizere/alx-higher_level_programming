a1ad720a3be27cdd804668ac8dcc7e42273ab861
bayizere <bayizeremarius119@gmail.com>
2023-10-04 03:33:52 -0700

    greater than

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 roma = abs(number) % 10
+
 if number < 0:
-roma  = -roma
+    roma = -roma
+
 print("Last digit of {} is {} and is ".format(number, roma), end="")
+
 if roma > 5:
-print("greater than 5")
+    print("greater than 5")
 elif roma == 0:
-print("0")
+    print("0")
 else:
-print("less than 6 and not 0")
+    print("less than 6 and not 0")
