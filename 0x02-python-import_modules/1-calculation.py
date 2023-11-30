9b705037a1646eeabf39e020853dcae958fc2459
bayizere <bayizeremarius119@gmail.com>
2023-11-30 23:13:56 +0200

    this last

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,12 @@
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
+
     x = 10
     y = 5
 
-    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
-    print("{:d} - {:d} = {:d}".format(x, y, sub(x, y)))
-    print("{:d} * {:d} = {:d}".format(x, y, mul(x, y)))
-    print("{:d} / {:d} = {:d}".format(x, y, div(x, y)))
+    print("{:g} + {:g} = {:g}".format(x, y, add(x, y)))
+    print("{:g} - {:g} = {:g}".format(x, y, sub(x, y)))
+    print("{:g} * {:g} = {:g}".format(x, y, mul(x, y)))
+    print("{:g} / {:g} = {:g}".format(x, y, div(x, y)))
 
