4395833e19bb286027e7f354830466308160db32
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-01-15 19:24:47 +0200

    addingfile

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -74,7 +74,7 @@ class Base:
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
-    
+
     @classmethod
     def create(cls, **dictionary):
         """Return a class instantied from a dictionary of attributes.
