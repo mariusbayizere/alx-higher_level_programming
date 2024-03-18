5c1b0307789ca6b834db46b6becf1f18ad12f0b3
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-03-18 04:04:50 +0200

    correct-file

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for betty in session.query(State).order_by(State.id):
-        for city_ins in betty.cities:
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
             print(city_ins.id, city_ins.name, sep=": ", end="")
-            print(" -> " + betty.name)
+            print(" -> " + instance.name)
