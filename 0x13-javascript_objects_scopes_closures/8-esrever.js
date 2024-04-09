c70bdaf5daf798e80eda42f0a50f31d42a0a6c22
mariusbayizere <bayizeremarius119@gmail.com>
2024-04-09 05:18:06 -0700

    pussh java script

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let air = list.length - 1;
-  let x = 0;
-  while ((air - x) > 0) {
-    const ape = list[air];
-    list[air] = list[x];
-    list[x] = ape;
-    air;
-    air--;
+  const revs = [];
+  for (const i of list) {
+    revs.unshift(i);
   }
-  return list;
+  return revs;
 };
