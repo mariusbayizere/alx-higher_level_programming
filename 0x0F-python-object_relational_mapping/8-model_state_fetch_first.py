cf7bf41314e693eb2ef71801f7744370e4e4e9bb
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-04-12 21:30:54 +0200

    search

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This module will printing the data from data base 
+"""This module will printing the data from data base
 """
 import sys
 from model_state import Base, State
@@ -8,8 +8,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
