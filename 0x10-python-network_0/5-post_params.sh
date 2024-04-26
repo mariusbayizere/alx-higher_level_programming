edcbf54e3bf45c7c23b76105030cfa338800a07b
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-04-26 16:01:44 +0200

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-# sends a POST request to the passed URL, and displays the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# This script sends a POST request to a specified URL and displays the response body
+# Usage: ./5-post_params.sh <URL>
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+URL=$1
+EMAIL="test@gmail.com"
+SUBJECT="I will always be here for PLD"
+
+curl -s -X POST "$URL" -d "email=$EMAIL&subject=$SUBJECT"
 
