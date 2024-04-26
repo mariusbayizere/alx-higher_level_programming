c2ffc4ac98ff94d9a30cec2db6170c2a5f8c11a0
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-04-26 14:40:29 +0200

    3-error_code.py

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body of the response
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 
