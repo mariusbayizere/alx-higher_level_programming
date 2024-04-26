0b653c2cf2427bc845a67a80c3ddec933bfca68a
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-04-26 16:08:38 +0200

    schellscript

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# This script sends a POST request to a specified URL and displays the response body
-# Usage: ./5-post_params.sh <URL>
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-
-curl -s -X POST "$URL" -d "email=$EMAIL&subject=$SUBJECT"
+# This script sends a POST request with the specified parameters and displays the response body
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
 
